4c56be1127cb76cc43f15349120f849baec81022
LINLIN MAO <24045143g@connect.polyu.hk>
2024-09-20 10:06:59 +0000

    modified:   temperature.py

--- temperature.py
+++ temperature.py
@@ -1,19 +1,28 @@
 import json
 import streamlit as st
-#想要将weather.json文件中的数据显示在网页上，首先要读取weather.json文件
-#read the file  weather.json
-#open the file
+
 with open('weather.json') as f:
     temperature = json.load(f)
-#将读取到的数据转换为字典
-temperature_dict = json.loads(temperature)
-#让用户通过选择框选择地区
-location = st.selectbox("Select a location", temperature_dict["temperature"]["data"])
-#根据用户选择的地区显示该地区的温度
-st.write("Temperature:", location["value"] + "°C")
-#以柱形图的方式显示所有地区温度
-st.bar_chart([float(location["value"]) for location in temperature_dict["temperature"]["data"])
-#以表格的方式显示所有地区温度
-st.write("## Temperature Table")
-st.write(temperature_dict["temperature"]["data"])
+
+#读取temperature里的所有place作为key，对应的温度作为其value，生成一个字典
+places = {region['place']: region['value'] for region in temperature['temperature']['data']}
+#将页面分成左右两边 
+
+left_column, right_column = st.columns(2)
+
+#在页面的左半边生成一个初始选择框，显示框内包含所有地区,默认选中第一个地区
+with left_column:
+     region = st.selectbox("Select a region", list(places.keys()), index=0)
+#显示所选地区的温度
+     st.write("Temperature in", region, "is", places[region],"°C")
+
+with right_column:
+#生成一个柱状图，显示所有地区的温度
+      st.write("Temperature distribution:")
+      st.bar_chart(places)
+
+
+
+
+
 
